6a6d03d706b9f92e396eae48a4524569be20e00f
Dan Lowe <dan@tangledhelix.com>
2010-10-20 11:02:37 -0400

    formatting cleanup

--- dml_article_thumb.php
+++ dml_article_thumb.php
@@ -1,30 +1,29 @@
 <?php
-// vim: foldmethod=marker:
 
 // run this file at the command line to produce a plugin for distribution:
 // $ php dml_article_thumb.php > dml_article_thumb-0.1.txt
 
 // Plugin name is optional.  If unset, it will be extracted from the current
 // file name. Uncomment and edit this line to override:
-# $plugin['name'] = 'abc_plugin';
+# $plugin[ 'name' ] = 'abc_plugin';
 
-$plugin['version']     = '0.6';
-$plugin['author']      = 'Dan Lowe';
-$plugin['author_uri']  = 'http://tangledhelix.com/';
-$plugin['description'] = 'Returns a thumbnail for the article image';
+$plugin[ 'version' ] = '0.6';
+$plugin[ 'author' ] = 'Dan Lowe';
+$plugin[ 'author_uri' ] = 'http://tangledhelix.com/archive/txp-dml-article-thumb';
+$plugin[ 'description' ] = 'Returns a thumbnail for the article image';
 
 // Plugin types:
 // 0 = regular plugin; loaded on the public web side only
 // 1 = admin plugin; loaded on both the public and admin side
 // 2 = library; loaded only when include_plugin() or require_plugin() is called
-$plugin['type'] = 0; 
+$plugin[ 'type' ] = 0; 
 
 
-@include_once('zem_tpl.php');
+@include_once( 'zem_tpl.php' );
 
 // {{{ plugin help
 
-if (0) {
+if ( 0 ) {
 ?>
 # --- BEGIN PLUGIN HELP ---
 
@@ -48,39 +47,44 @@ ID (see the Advanced area in the write tab), then use:</p>
 
 // {{{ dml_article_thumb()
 
-function dml_article_thumb($atts)
-{
+function dml_article_thumb( $atts ) {
     global $thisarticle, $img_dir;
 
-    extract(lAtts(array(
+    extract( lAtts( array(
         'style' => '',
         'align' => ''
-    ),$atts));
+    ) ,$atts ) );
 
-    $theimage = ($thisarticle['article_image'])
-                        ? $thisarticle['article_image']
-                        : '';
+    $theimage = '';
+    if ( $thisarticle[ 'article_image' ] ) {
+        $theimage = $thisarticle[ 'article_image' ];
+    }
 
-    if ($theimage)
-    {
-        if (is_numeric($theimage))
-        {
-            $rs = safe_row('*', 'txp_image', "id='$theimage'");
-            if ($rs)
-            {
-                extract($rs);
+    if ( $theimage ) {
+        if ( is_numeric( $theimage ) ) {
+            $rs = safe_row( '*', 'txp_image', "id='$theimage'" );
+            if ( $rs ) {
+                extract( $rs );
                 $out = '<img src="'. hu . "$img_dir/${id}t$ext\"";
 
-                if (!empty($alt))   $out .= ' alt="'   . $alt   . '"';
-                if (!empty($style)) $out .= ' style="' . $style . '"';
-                if (!empty($align)) $out .= ' align="' . $align . '"';
+                if ( !empty( $alt ) ) {
+                    $out .= " alt=\"$alt\"";
+                }
+                if ( !empty( $style ) ) {
+                    $out .= " style=\"$style\"";
+                }
+                if ( !empty( $align ) ) {
+                    $out .= " align=\"$align\"";
+                }
 
                 $out .= ' />';
 
                 return $out;
             }
+
+        } else {
+            return "<img src=\"$theimage\" alt=\"\" />";
         }
-        else return '<img src="' . $theimage . '" alt="" />';
     }
 }
 
